fcf0a50a1a6806a3f95e266dada2eacfaa4124b9
pyadminru <zh.vitaliy92@yandex.ru>
2025-09-18 23:43:45 +0300

    Формирование проекта

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Монтируем статические файлы
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+# Создаем экземпляр Jinja2Templates
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def index(request: Request):
+    # Передаем контекст в шаблон
+    return templates.TemplateResponse("index.html", {"request": request})
